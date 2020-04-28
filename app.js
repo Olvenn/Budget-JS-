502da49a3497fb878a9f06bc0a3f0cabce84e001
Olvenn <olga-vencel@yandex.ru>
2020-04-28 18:01:43 +0300

    start  buget

---
+++ app.js
@@ -0,0 +1,78 @@
+//BUDGET CONTROLLER
+
+let budgetController = (function() {
+
+
+})();
+
+//UT CONTROLLER
+let uIController = (function() {
+
+})();
+
+
+//GLOBAL APP CONTROLLER
+let controller = (function(budgetCtrl, uICtrl) {
+
+    var ctrlAddItem = function(event, el) {
+        
+    }
+ 
+    let addBtn = document.querySelector(".add__btn");
+    addBtn.addEventListener("click", function() {
+        console.log("AddBtn");
+    });
+
+            // 1. Get the field input data
+
+            // 2. Add the item to the budget controller
+
+            // 3. Add the item to the UI
+
+            // 4. Clear the fields
+
+            // 5. Calculate and update budget
+
+            // 6. Calculate and update percentages
+
+
+
+
+})(budgetController, uIController);
+
+        // 1. Calculate the budget
+
+        // 2. Return the budget
+
+        // 3. Display the budget on the UI
+
+        // 1. Calculate percentages
+
+        // 2. Read percentages from the budget controller
+
+        // 3. Update the UI with the new percentages
+
+        // 1. Get the field input data
+
+
+            // 2. Add the item to the budget controller
+
+            // 3. Add the item to the UI
+
+            // 4. Clear the fields
+
+            // 5. Calculate and update budget
+
+            // 6. Calculate and update percentages
+
+
+            //inc-1
+
+            
+            // 1. delete the item from the data structure
+
+            // 2. Delete the item from the UI
+
+            // 3. Update and show the new budget
+
+            // 4. Calculate and update percentages
